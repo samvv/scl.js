ed91c35d4f21cd4ae19c933a4623bdd63cc852e5
Sam Vervaeck <sam@vervaeck.be>
2016-06-27 17:58:22 +0200

    Export missing containers

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
 
 export * from "./interfaces/Container"
+export * from "./interfaces/UnorderedContainer"
+export * from "./interfaces/OrderedContainer"
+
+export * from "./interfaces/List"
+export * from "./src/SingleLinkedList"
+export * from "./src/DoubleLinkedList"
+
 export * from "./interfaces/Queue"
+
 export * from "./interfaces/Dict"
+//export * from "./src/HashDict"
+
 export * from "./interfaces/Set"
-export * from "./interfaces/NamedSet"
+export * from "./src/HashSet"
+
+export * from "./src/NamedSet"
+
