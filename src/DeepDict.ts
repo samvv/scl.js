2e3f26853f4a0ab0dcdd529dff96fff48617abd0
Sam Vervaeck <sam@vervaeck.be>
2016-06-30 18:25:47 +0200

    Add full functionality to DeepDict

--- src/DeepDict.ts
+++ src/DeepDict.ts
@@ -6,7 +6,27 @@ import { Pair, Dict } from "../interfaces/Dict"
  */
 export class DeepDict<K, V> implements Dict<K[], V> {
 
-  rootNode: any = {}
+  rootNode: any = {};
+
+  [Symbol.iterator]() {
+    return this.iterator()
+  }
+
+  clear() {
+    this.rootNode = {}
+  }
+
+  iterator(): Iterator<Pair<K[], V>> {
+    return new class {
+      next() {
+        throw new Error(`not implemented`)
+      }
+    }
+  }
+
+  has(pair: Pair<K[], V>) {
+    return this.hasKey(pair.key)
+  }
 
   hasKey(path: K[]) {
     let node = this.rootNode
@@ -18,11 +38,24 @@ export class DeepDict<K, V> implements Dict<K[], V> {
     return true
   }
 
-  hasValue(value: V) {
+  removeValues(value: V) {
     throw new Error(`not implemented`)
   }
 
-  get(path: K[]) {
+  removeKey(path: K[]) {
+    let node = this.rootNode
+    for (const chunk of path.slice(1)) {
+      node = node[chunk]
+      if (!node)
+        throw new Error(`value not found for ${path}`)
+    }
+  }
+
+  hasValue(value: V) {
+    throw new Error(`not implemented`)
+  }
+  
+  getValue(path: K[]) {
     let node = this.rootNode
     for (const chunk of path) {
       node = node[chunk]
@@ -45,5 +78,6 @@ export class DeepDict<K, V> implements Dict<K[], V> {
     }
     node[path[path.length - 1]] = el
   }
+
 }
 
