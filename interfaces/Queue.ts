3d51507bb623ad6ccc2b43cd9e63dd499cc8b9ff
Sam Vervaeck <sam@vervaeck.be>
2016-06-30 21:24:13 +0200

    Update Queue

--- interfaces/Queue.ts
+++ interfaces/Queue.ts
@@ -1,9 +1,22 @@
 
 import { Container } from "./Container"
 
+/**
+ * A queue is a kind of container of which the insertion order is guaranteed to
+ * be reversely satisfied.
+ */
 export interface Queue<T> extends Container<T> {
-  push(el: T)
+  /**
+   * Add an element to the queue.
+   */
+  enqueue(el: T)
+  /**
+   * Get the element that is at the beginning of the queue, but do not remove it.
+   */
   peek(el: T)
-  pop(el: T)
+  /**
+   * Give the first element of the queue, removing the element in the process.
+   */
+  dequeue()
 }
 
