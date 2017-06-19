11bcb74ee41aba84ab90df436bbcb1f885b2ed99
Sam Vervaeck <sam@vervaeck.be>
2017-06-19 19:33:23 +0200

    Add rest() to list interface

--- interfaces/List.d.ts
+++ interfaces/List.d.ts
@@ -7,5 +7,7 @@ import { OrderedContainer } from "./Ordered"
  * same kind. A list is characterized by low-cost insertion of elements, while
  * referencing an element at a given position is generally slower.
  */
-export interface List<T> extends MultiContainer<T>, OrderedContainer<T> { }
+export interface List<T> extends MultiContainer<T>, OrderedContainer<T> {
+  rest(): List<T>
+}
 
