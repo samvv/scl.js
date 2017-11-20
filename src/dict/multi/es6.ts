2e7932c51884eef2784c38b916566f24235a8e73
Sam Vervaeck <sam@vervaeck.be>
2017-11-20 22:36:27 +0100

    Fix size() for ES6 multi map

--- src/dict/multi/es6.ts
+++ src/dict/multi/es6.ts
@@ -3,6 +3,7 @@ import { Pair, MultiDict } from "../../../interfaces"
 
 export class ESMultiDict<K, V> implements MultiDict<K, V> {
   
+  _size = 0;
   _map = new Map<K, Set<V>>()
 
   add([key, val]) {
@@ -10,7 +11,7 @@ export class ESMultiDict<K, V> implements MultiDict<K, V> {
   }
 
   size() {
-    return this._map.size
+    return this._size;
   }
 
   count([key, value]: Pair<K, V>) {
@@ -45,7 +46,10 @@ export class ESMultiDict<K, V> implements MultiDict<K, V> {
     if (!this._map.has(key))
       this._map.set(key, new Set<V>())
     const s = this._map.get(key)
-    s.add(value)
+    if (!s.has(value)) {
+      s.add(value)
+      ++this._size;
+    }
   }
 
   hasValue(val: V) {
@@ -67,31 +71,43 @@ export class ESMultiDict<K, V> implements MultiDict<K, V> {
 
   clear() {
     this._map.clear()
+    this._size = 0;
   }
 
   deleteValue(val: V) {
     for (const [key, s] of this._map) {
       if (s.has(val)) {
         s.delete(val)
+        --this._size;
         break
       }
     }
   }
 
   deleteValues(val: V) {
-    for (const [key, s] of this._map)
-      s.delete(val)
+    for (const [key, s] of this._map) {
+      if (s.has(val)) {
+        s.delete(val);
+        --this._size;
+      }
+    }
   }
 
   deleteAll([key, val]: Pair<K, V>) {
     const s = this._map.get(key)
     if (s === undefined)
       return
-    s.delete(val)
+    if (s.has(val)) {
+      s.delete(val)
+      --this._size;
+    }
   }
 
   deleteKeys(key: K) {
-    this._map.delete(key)
+    if (this._map.has(key)) {
+      this._size -= this._map.get(key).size;
+      this._map.delete(key)
+    }
   }
 
   delete([key, value]: Pair<K, V>) {
@@ -100,7 +116,10 @@ export class ESMultiDict<K, V> implements MultiDict<K, V> {
       throw new Error(`key ${key} not found`)
     if (!s.has(value))
       throw new Error(`value ${value} not found`)
-    s.delete(value)
+    if (s.has(value)) {
+      --this._size;
+      s.delete(value)
+    }
   }
 
 }
