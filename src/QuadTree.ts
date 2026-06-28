d316a3421ff7185109ab760e62378b0dd17bd5f0
Sam Vervaeck <samvv@pm.me>
2026-06-28 04:46:55 +0200

    Move Vec2 namespace to math.ts

--- src/QuadTree.ts
+++ src/QuadTree.ts
@@ -1,19 +1,6 @@
 import { AddResult, Cursor, Index, Range } from "./interfaces.js";
 import { isEqual, todo } from "./util.js";
-
-export type Vec2 = [x: number, y: number];
-
-export namespace Vec2 {
-
-  export function isStrictEqual(a: Vec2, b: Vec2): boolean {
-    return a[0] === b[0] && a[1] === b[1];
-  }
-
-  export function distance([x0, y0]: Vec2, [x1, y1]: Vec2): number {
-    return Math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2);
-  }
-
-}
+import { Vec2 } from "./math.js";
 
 type Value<T> = [
   position: Vec2,
---
+++ src/math.ts
@@ -0,0 +1,15 @@
+
+export type Vec2 = [x: number, y: number];
+
+export namespace Vec2 {
+
+  export function isStrictEqual(a: Vec2, b: Vec2): boolean {
+    return a[0] === b[0] && a[1] === b[1];
+  }
+
+  export function distance([x0, y0]: Vec2, [x1, y1]: Vec2): number {
+    return Math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2);
+  }
+
+}
+
